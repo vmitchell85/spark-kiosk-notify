9c91dbdb481e5b2c65e5696342a13b05de4b5b71
Vince Mitchell <vincent.mitchell@gmail.com>
2016-04-20 22:48:04 -0400

    rename file

---
+++ src/SparkKioskNotifyServiceProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace vmitchell85\SparkKioskNotify;
+
+use Illuminate\Support\ServiceProvider;
+
+class SparkKioskNotifyServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->loadViewsFrom(__DIR__.'/resources/views/', 'spark-kiosk-notify');
+
+        $this->publishes([
+            __DIR__.'/models/' => base_path('app/'),
+            __DIR__.'/resources/assets/js/' => base_path('resources/assets/js/components/'),
+            __DIR__ . '/resources/views/' => base_path('resources/views/vendor/spark-kiosk-notify/'),
+        ]);
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        if (! $this->app->routesAreCached()) {
+            require __DIR__.'/routes.php';
+        }
+    }
+}
